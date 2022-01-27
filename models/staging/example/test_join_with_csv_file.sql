b726825266776d07545f13cd1ddc4832bc3c9a3c
mixeil <suxoza@gmail.com>
2022-01-27 13:59:19 +0400

    joins, first try

---
+++ models/staging/example/test_join_with_csv_file.sql
@@ -0,0 +1,23 @@
+
+with from_existing_table as (
+    select * from {{ ref("just_example_file") }}
+),
+
+from_csv_file as (
+    select * from {{ ref("test_file_import") }}
+),
+
+final as (
+
+    select 
+        from_csv_file.name,
+        from_csv_file.surname,
+        from_csv_file.email,
+        from_csv_file.ss_item_sk as first_ss_item_sk,
+        from_existing_table.ss_item_sk
+    from from_csv_file
+    left join from_existing_table on from_existing_table.ss_item_sk = from_csv_file.ss_item_sk 
+
+)
+
+select * from final
